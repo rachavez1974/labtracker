bc2e157a4f538fe76ee74e0a1448754e727cb180
Ramon Chavez <rachavez1974@gmail.com>
2019-08-05 02:16:36 -0400

    Add new to student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,5 +3,15 @@ class StudentsController < ApplicationController
   get '/signup' do
     erb :'students/new'
   end
+
+  post '/signup' do 
+    @student = Student.new(params[:student])
+
+      if @student.save
+        flash[:message] = "Your account was created successfully!"
+      else
+        erb :'students/new'
+      end
+  end
   
 end

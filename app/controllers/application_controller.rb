f65d24c4c0991c564775c66dd9f80c0722856c24
Ramon Chavez <rachavez1974@gmail.com>
2019-08-05 01:53:39 -0400

    Add flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,18 @@
 require './config/environment'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    use Rack::Flash
     
   end
 
   get "/" do
-    erb :welcome
+    erb :'static_pages/home.html'
   end
 
 end

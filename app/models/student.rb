3201cb8bf7765407e6e6b9d5dd82b6047c1f70cc
Ramon Chavez <rachavez1974@gmail.com>
2019-08-06 17:19:14 -0400

    Add enum for class_year

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,9 @@
 class Student < ActiveRecord::Base
+
+  enum class_year: [:Freshman, :Sophomore, :Junior, :Senior]
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   has_secure_password
+  validates :name, presence: true
   validates :password, presence: true, length: {minimum: 3}, allow_nil: true
   validates :user_name, presence: true, length: {maximum: 255}, 
                            format: {with: VALID_EMAIL_REGEX}, 

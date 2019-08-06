4424c6bf14fbccc7fb9581648223372e6b55a11c
Ramon Chavez <rachavez1974@gmail.com>
2019-08-06 14:17:04 -0400

    Add more validations to Student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,9 @@
 class Student < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   has_secure_password
-  validates_presence_of :password
-  validates_presence_of :user_name, :class_year
+  validates :password, presence: true, length: {minimum: 3}, allow_nil: true
+  validates :user_name, presence: true, length: {maximum: 255}, 
+                           format: {with: VALID_EMAIL_REGEX}, 
+                           uniqueness: {case_sensitive: false}
+  validates :class_year, presence: true
 end

bc011323b8cf4755c920c5de771c9a94baf67b0d
Ramon Chavez <rachavez1974@gmail.com>
2019-08-08 12:17:52 -0400

    Can create labs wit teachers simultaneously

--- app/controllers/labs_controller.rb
+++ app/controllers/labs_controller.rb
@@ -36,12 +36,15 @@ class LabsController < ApplicationController
   post "/labs" do
     if Sessions.is_logged_in?(session)
       @lab = Lab.new(params[:lab])
+      @teacher = Teacher.create(params[:teacher])
+      @lab.teacher_id = @teacher.id
       @lab.student_id = @current_student.id
         if @lab.save
           flash[:message] = "Your lab was created successfully!"
           redirect "/labs/#{@lab.id}"
         else
-          @errors = @lab.errors
+          #merge both errors from both models
+          @errors = @lab.errors.merge(@teacher.errors)
           erb :'/labs/new.html'
         end
     else
